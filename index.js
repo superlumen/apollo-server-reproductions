48ddfc2b857c9d2d1ee5b3bb6090eda1ed0eae0d
Callum Macdonald <git@callum-macdonald.com>
2018-04-25 19:57:16 +0200

    Prettier.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
 
 // Some fake data
 const books = [
   {
     title: "Harry Potter and the Sorcerer's stone",
-    author: 'J.K. Rowling',
+    author: "J.K. Rowling"
   },
   {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
+    title: "Jurassic Park",
+    author: "Michael Crichton"
+  }
 ];
 
 // The GraphQL schema in string form
@@ -23,25 +23,25 @@ const typeDefs = `
 
 // The resolvers
 const resolvers = {
-  Query: { books: () => books },
+  Query: { books: () => books }
 };
 
 // Put together a schema
 const schema = makeExecutableSchema({
   typeDefs,
-  resolvers,
+  resolvers
 });
 
 // Initialize the app
 const app = express();
 
 // The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
 
 // GraphiQL, a visual editor for queries
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
 // Start the server
 app.listen(3000, () => {
-  console.log('Go to http://localhost:3000/graphiql to run queries!');
+  console.log("Go to http://localhost:3000/graphiql to run queries!");
 });
